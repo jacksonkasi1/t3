d2671f89f2393c9de8298a9a95f078a9eebd2b39
jacksonkasi1 <jacksonkasipeacock@gmail.com>
2023-10-25 16:10:44 +0000

    update

---
+++ todo/src/app/api/trpc-playground.ts
@@ -0,0 +1,51 @@
+/* eslint-disable @typescript-eslint/require-await */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { appRouter } from '~/server/api/root'
+import { NextApiHandler, NextApiRequest, NextApiResponse } from 'next'
+import { nextHandler } from 'trpc-playground/handlers/next'
+
+interface SetupHandlerOptions {
+  router: typeof appRouter;
+  trpcApiEndpoint: string;
+  playgroundEndpoint: string;
+  polling: {
+    interval: number;
+  };
+  renderOptions: {
+    cdnUrl: string;
+    version: string | null;
+  };
+  request: {
+    superjson: boolean;
+  };
+}
+
+const setupHandler = async (): Promise<NextApiHandler> => {
+  const options: SetupHandlerOptions = {
+    router: appRouter,
+    trpcApiEndpoint: '/api/trpc',
+    playgroundEndpoint: '/api/trpc-playground',
+    polling: {
+      interval: 4000,
+    },
+    renderOptions: {
+      cdnUrl: 'http://localhost:45245',
+      version: null,
+    },
+    request: {
+      superjson: true,
+    },
+  };
+
+  const handler = nextHandler(options);
+  return handler;
+};
+
+const handler: NextApiHandler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const playgroundHandler = await setupHandler();
+  await playgroundHandler(req, res);
+};
+
+export default handler;
